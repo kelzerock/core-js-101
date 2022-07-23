5c96db0832f462263b2ec4c45401117981984151
Aleksei Zhuchkov <kelzzzer@gmail.com>
2022-07-23 23:06:38 +0300

    complete 25 task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -96,8 +96,9 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(/* a, b */) {
-  throw new Error('Not implemented');
+function getLinearEquationRoot(a = 0, b = 0) {
+  // throw new Error('Not implemented');
+  return -b / a;
 }
 
 
@@ -135,8 +136,9 @@ function getAngleBetweenVectors(/* x1, y1, x2, y2 */) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  // throw new Error('Not implemented');
+  return value % 10;
 }
 
 
@@ -151,8 +153,9 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  // throw new Error('Not implemented');
+  return parseFloat(value);
 }
 
 /**
@@ -168,8 +171,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  // throw new Error('Not implemented');
+  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
 }
 
 
@@ -190,8 +194,17 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  // throw new Error('Not implemented');
+  const powFor = Number(1 + '0'.repeat(pow));
+  const x = num % powFor;
+  const newNum = num - x;
+  const str = x.toString();
+  const number = 1;
+  if (Number(str[0]) < 5) {
+    return newNum;
+  }
+  return newNum + Number(number + '0'.repeat(str.length));
 }
 
 /**
